cdf4d553e9ac5414dd32c060805aa389cb6ceed5
Rahul Panda <rahulpanda4349@gmail.com>
2026-09-16 14:55:55 +0530

    add a button type

--- 06_currencyConvertor/src/App.jsx
+++ 06_currencyConvertor/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
               />
             </div>
             <div className='relative w-full h-0.5'>
-              <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 boarder-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5' onClick={swap}>
+              <button type="button" className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5' onClick={swap}>
               Swap </button>
             </div>
             <div className='w-full mb-1'>
